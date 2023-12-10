dc6053a58fc4bca4efcb5f72a508770affa663ee
Adil Farooque <Adilfarq11@gmail.com>
2023-12-10 22:21:23 +0530

    Finished the pass or fail program

--- making_the_grade/pass_file.go
+++ making_the_grade/pass_file.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func PassOrFail() {
@@ -18,5 +20,24 @@ func PassOrFail() {
 		//Report the error and stop the program.
 		log.Fatal(err)
 	}
-	fmt.Println(input)
+	//Trim the newline charachter from the inpout string.
+	input = strings.TrimSpace(input)
+	//Convert the string to a float64 value.
+	grade, err := strconv.ParseFloat(input, 64)
+	//Just as with ReadString report any error when converting.
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*Declare the "status" variable here, so
+	it's in scope for the rest of the function*/
+	var status string
+	//Compare to the float64 in "grade", not the string in input.
+	if grade >= 60 {
+		status = "Passed"
+	} else {
+		status = "Failed"
+	}
+	//The pass or fail status
+	fmt.Println("A greade of", grade, "is", status)
+
 }
