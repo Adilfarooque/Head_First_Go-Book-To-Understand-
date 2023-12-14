f96a4c303db0dd711503f89f900195787a3ad2a4
Adil Farooque <Adilfarq11@gmail.com>
2023-12-14 22:45:30 +0530

    Comparing the guess to the target

--- guss_game/guss.go
+++ guss_game/guss.go
@@ -1,8 +1,13 @@
 package gussgame
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 	/*
 		Import Path       package name
@@ -14,15 +19,38 @@ import (
 
 func Guss() {
 	/*To get different random numbers, we need to pass a value to the rand.Seed function
-	That will "seed" random number generator 
+	That will "seed" random number generator
 	*/
 	//Get the current date and time, as an integer.
-	seconds:=time.Now().UnixNano()
+	seconds := time.Now().UnixNano()
 	rand.New(rand.NewSource(seconds))
 	//Call rand.Intn to generated numbers should be different each time!
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen a random number b/w 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
 
+	//Create a bufio Reader which lets us read keyboard input
+	reader := bufio.NewReader(os.Stdin)
+	//Ask a number
+	fmt.Print("Make a guess: ")
+	//Read what the user types up until they press Enter.
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Remove the newline
+	input = strings.TrimSpace(input)
+	//convert the input string to an integer
+	guess, err := strconv.Atoi(input)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	if guess < target {
+		fmt.Println("OOps. Your guess was LOW.")
+	}else if guess > target {
+		fmt.Println("OOps. Your guess was HIGH.")
+	}
+	
+	fmt.Printf("Our guss is:- %d",target)
 }
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	call "Headfirstgo/calling_method"
+	callingmethod "Headfirstgo/calling_method"
 	gussgame "Headfirstgo/guss_game"
 	makingthegrade "Headfirstgo/making_the_grade"
 )
 
 func main() {
 	//call.CallingMehod()
-	call.Gorocks()
+	callingmethod.Gorocks()
 	makingthegrade.PassOrFail()
 	gussgame.Guss()
 }
