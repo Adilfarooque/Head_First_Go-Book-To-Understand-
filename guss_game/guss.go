e766d56feba12db7108215b31627bbee35ff8878
Adil Farooque <Adilfarq11@gmail.com>
2023-12-15 21:48:46 +0530

    Allow them to guess more

--- guss_game/guss.go
+++ guss_game/guss.go
@@ -18,39 +18,43 @@ import (
 	*/)
 
 func Guss() {
-	/*To get different random numbers, we need to pass a value to the rand.Seed function
-	That will "seed" random number generator
-	*/
-	//Get the current date and time, as an integer.
-	seconds := time.Now().UnixNano()
-	rand.New(rand.NewSource(seconds))
-	//Call rand.Intn to generated numbers should be different each time!
-	target := rand.Intn(100) + 1
-	fmt.Println("I've chosen a random number b/w 1 and 100.")
-	fmt.Println("Can you guess it?")
+	for i := 0; i < 10; i++ {
+		/*To get different random numbers, we need to pass a value to the rand.Seed function
+		That will "seed" random number generator
+		*/
+		//Get the current date and time, as an integer.
+		seconds := time.Now().UnixNano()
+		rand.New(rand.NewSource(seconds))
+		//Call rand.Intn to generated numbers should be different each time!
+		target := rand.Intn(100) + 1
+		fmt.Println("")
+		fmt.Println("I've chosen a random number b/w 1 and 100.")
+		fmt.Println("Can you guess it?")
 
-	//Create a bufio Reader which lets us read keyboard input
-	reader := bufio.NewReader(os.Stdin)
-	//Ask a number
-	fmt.Print("Make a guess: ")
-	//Read what the user types up until they press Enter.
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	//Remove the newline
-	input = strings.TrimSpace(input)
-	//convert the input string to an integer
-	guess, err := strconv.Atoi(input)
-	if err != nil{
-		log.Fatal(err)
-	}
+		//Create a bufio Reader which lets us read keyboard input
+		reader := bufio.NewReader(os.Stdin)
+		//Ask a number
+		fmt.Print("Make a guess: ")
+		//Read what the user types up until they press Enter.
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		//Remove the newline
+		input = strings.TrimSpace(input)
+		//convert the input string to an integer
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if guess < target {
-		fmt.Println("OOps. Your guess was LOW.")
-	}else if guess > target {
-		fmt.Println("OOps. Your guess was HIGH.")
+		if guess < target {
+			fmt.Println("OOps. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("OOps. Your guess was HIGH.")
+		}
+
+		fmt.Printf("Our guss is:- %d", target)
 	}
-	
-	fmt.Printf("Our guss is:- %d",target)
+	fmt.Println("")
 }
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	callingmethod "Headfirstgo/calling_method"
 	gussgame "Headfirstgo/guss_game"
-	makingthegrade "Headfirstgo/making_the_grade"
 )
 
 func main() {
-	//call.CallingMehod()
-	callingmethod.Gorocks()
-	makingthegrade.PassOrFail()
+	//callingmethod.CallingMehod()
+	//callingmethod.Gorocks()
+	//makingthegrade.PassOrFail()
 	gussgame.Guss()
 }
