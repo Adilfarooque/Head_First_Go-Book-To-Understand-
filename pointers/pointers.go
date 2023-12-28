2dcacc125f8e3ddafcb7e76723fa7651ca1d9160
Adil Farooque <Adilfarq11@gmail.com>
2023-12-28 22:01:20 +0530

    Using reflect.TypeOF package to get the type of the variable

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,21 +1,35 @@
 package pointers
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func Pointers() {
+	/*
+		var myInt int
+		fmt.Println(&myInt)
+
+		var myFloat float64
+		fmt.Println(&myFloat)
+
+		var myBool bool
+		fmt.Println(&myBool)
+
+
+		amount := 6
+		fmt.Println(amount)
+		fmt.Println(&amount)
+	*/
+
+	// Pointer types
 
 	var myInt int
-	fmt.Println(&myInt)
+	fmt.Println(reflect.TypeOf(&myInt))
 
 	var myFloat float64
-	fmt.Println(&myFloat)
+	fmt.Println(reflect.TypeOf(&myFloat))
 
 	var myBool bool
-	fmt.Println(&myBool)
-
-
-	amount := 6
-	fmt.Println(amount)
-	fmt.Println(&amount)
-
+	fmt.Println(reflect.TypeOf(&myBool))
 }
